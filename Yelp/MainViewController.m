c6cb00d4a53d5ee65530ac163012404a43e2fd25
Tejas Lagvankar <advantej@yahoo-inc.com>
2015-01-28 12:06:42 -0800

    Basic list view

--- Yelp/MainViewController.m
+++ Yelp/MainViewController.m
@@ -8,6 +8,8 @@
 
 #import "MainViewController.h"
 #import "YelpClient.h"
+#import "Business.h"
+#import "BusinessCell.h"
 
 //NSString * const kYelpConsumerKey = @"vxKwwcR_NMQ7WaEiQBK_CA";
 //NSString * const kYelpConsumerSecret = @"33QCvh5bIF5jIHR5klQr7RtBDhQ";
@@ -19,9 +21,11 @@ NSString * const kYelpConsumerSecret = @"7zJYu2HH3pwoBzhC2jzYc4REsz0";
 NSString * const kYelpToken = @"BGwL_k-sXGr1vru5ZjmzUKOGuhqEiNoB";
 NSString * const kYelpTokenSecret = @"Utpn9xUj9Y8uVYwDxh2rG2xlWvk";
 
-@interface MainViewController ()
+@interface MainViewController () <UITableViewDelegate, UITableViewDataSource>
 
 @property (nonatomic, strong) YelpClient *client;
+@property (nonatomic, strong) NSArray *businesses;
+@property (weak, nonatomic) IBOutlet UITableView *tableView;
 
 @end
 
@@ -35,7 +39,14 @@ NSString * const kYelpTokenSecret = @"Utpn9xUj9Y8uVYwDxh2rG2xlWvk";
         self.client = [[YelpClient alloc] initWithConsumerKey:kYelpConsumerKey consumerSecret:kYelpConsumerSecret accessToken:kYelpToken accessSecret:kYelpTokenSecret];
         
         [self.client searchWithTerm:@"Thai" success:^(AFHTTPRequestOperation *operation, id response) {
-            NSLog(@"response: %@", response);
+            //NSLog(@"response: %@", response);
+
+            NSArray *businessesDictionaries = response[@"businesses"];
+            self.businesses = [Business businessesWithDictionaries:businessesDictionaries];
+
+            [self.tableView reloadData];
+
+
         } failure:^(AFHTTPRequestOperation *operation, NSError *error) {
             NSLog(@"error: %@", [error description]);
         }];
@@ -46,7 +57,26 @@ NSString * const kYelpTokenSecret = @"Utpn9xUj9Y8uVYwDxh2rG2xlWvk";
 - (void)viewDidLoad
 {
     [super viewDidLoad];
-    // Do any additional setup after loading the view from its nib.
+    self.tableView.delegate = self;
+    self.tableView.dataSource = self;
+    [self.tableView registerNib:[UINib nibWithNibName:@"BusinessCell" bundle:nil] forCellReuseIdentifier:@"BusinessCell"];
+
+    self.tableView.rowHeight = UITableViewAutomaticDimension;
+
+    self.title = @"Yelp";
+}
+
+- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+    return self.businesses.count;
+}
+
+- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+
+
+    BusinessCell *cell = [self.tableView dequeueReusableCellWithIdentifier:@"BusinessCell"];
+    cell.business = self.businesses[indexPath.row];
+
+    return cell;
 }
 
 - (void)didReceiveMemoryWarning
